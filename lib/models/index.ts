1f83b9184e1a80c6aad4c36a1c41f21b1b4ab1cf
iverly <iverly.dev@gmail.com>
2022-01-07 18:22:01 +0100

    feat: add registerExtraModels method

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { Sequelize } from 'sequelize';
+import { Model, Sequelize } from 'sequelize';
 
 import { User } from '@/models/user';
 
@@ -7,23 +7,28 @@ export const models = {
   User,
 };
 
-export const registerModels = async (fastify: FastifyInstance, sequelize: Sequelize) => {
-  Object.keys(models).forEach((key: any) => {
-    const model = models[key];
-
+export const registerExtraModels = async (
+  m: Model[],
+  fastify: FastifyInstance,
+  sequelize: Sequelize,
+) => {
+  for (const model of (m as any)) {
     fastify.log.debug({ name: 'sequelize' }, 'Registering model: %s', model.name);
     model.fn(sequelize);
-  });
+  }
 
-  Object.keys(models).forEach((key: any) => {
-    const model = models[key];
-
+  for (const model of (m as any)) {
     if (model.associate) {
       fastify.log.debug({ name: 'sequelize' }, 'Execute associate method for model: %s', model.name);
       model.associate();
     }
-  });
+  }
 };
+
+export const registerModels = async (
+  fastify: FastifyInstance,
+  sequelize: Sequelize,
+) => registerExtraModels(Object.keys(models).map((key) => models[key]), fastify, sequelize);
 
 export * from '@/models/user';
 export default models;
