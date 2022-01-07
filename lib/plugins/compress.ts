e8ba03b478d13bed5635462c0bfd3262857eeab3
iverly <iverly.dev@gmail.com>
2022-01-07 17:18:58 +0100

    feat: add compress fastify plugin

---
+++ lib/plugins/compress.ts
@@ -0,0 +1,6 @@
+import fp from 'fastify-plugin';
+import compress, { FastifyCompressOptions } from 'fastify-compress';
+
+export default fp<FastifyCompressOptions>(async (fastify) => {
+  fastify.register(compress);
+});
